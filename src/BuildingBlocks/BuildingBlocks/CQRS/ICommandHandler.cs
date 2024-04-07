ed4192f64d796fb3a080e2202da2d67eef07d660
kodoku-x <datsunbae@gmail.com>
2024-04-07 09:54:26 +0700

    add building blocks

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TRequest>
+    : ICommandHandler<TRequest, Unit>
+    where TRequest : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TRequest, TResponse>
+    : IRequestHandler<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
