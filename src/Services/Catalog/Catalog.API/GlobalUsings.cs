8512d8cf98b3efe2be207b525cd2823b0011e994
kodoku-x <datsunbae@gmail.com>
2024-04-08 19:35:23 +0700

    completed catalog microservice

---
+++ src/Services/Catalog/Catalog.API/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using Marten;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using Catalog.API.Exceptions;
+global using Catalog.API.Models;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Data;
+
