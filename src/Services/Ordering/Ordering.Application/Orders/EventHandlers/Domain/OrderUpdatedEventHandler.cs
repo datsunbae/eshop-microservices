9933d7164560fac0f49bcc27cb795aea5afe0fa4
kodoku-x <datsunbae@gmail.com>
2024-04-11 17:39:08 +0700

    feat: ordering microservice

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEvent> logger) : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Events/OrderCreatedEvent.cs
@@ -0,0 +1,5 @@
+﻿using Ordering.Domain.Modes;
+
+namespace Ordering.Domain.Events;
+
+public record OrderCreatedEvent(Order Order) : IDomainEvent;
